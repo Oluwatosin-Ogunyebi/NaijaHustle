b2792b28b3b664601d7b8dae5784f171d950f1e0
Oluwatosin Ogunyebi <oluwatosinogunyebi@gmail.com>
2020-02-19 08:32:25 +0100

    Export File for Android

---
+++ NaijaHustle/android/app/src/main/java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,11 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "TCv8cpr7slodef5wyfm2Whd2/yGZ/bVRGyisI56s5F5Pe/p3la20XBh58HDNq7wLHXurIZWm4AgaK/wnmqq8DQ==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+    public static native String jsSettingsString();
+}
